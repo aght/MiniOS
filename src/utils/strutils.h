5928eabfcaa84ffee27bb124eee990e219d3d35a
Andy Tang <andytang43@gmail.com>
2019-05-07 18:28:39 -0700

    moved some vector string functions to strutils

--- src/utils/strutils.h
+++ src/utils/strutils.h
@@ -5,7 +5,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "../vector/vector.h"
+
 bool stricmp(const char *a, const char *b);
 int strreplc(char *str, char oldc, char newc);
+char *vtostr(vector *vec, char buffer[]);
+int vstrrcc(vector *str, char c);
 
 #endif
