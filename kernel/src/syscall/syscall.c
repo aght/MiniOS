ac4f83eed157ae12377ab0917dcb634fbdb5d9cc
Andy Tang <andytang43@gmail.com>
2019-05-12 14:38:33 -0700

    added handling for \n, \r, \b for syscall

--- kernel/src/syscall/syscall.c
+++ kernel/src/syscall/syscall.c
@@ -5,8 +5,19 @@
 
 void *c_svc_handler(int svc_number, void *r1, void *r2, void *r3, void *r4) {
     switch (svc_number) {
-        case 0:
-            return console_print("%c", r1);
+        case 0: {
+            switch ((char)r1) {
+                case '\n':
+                case '\r':
+                    console_newline();
+                    break;
+                case '\b':
+                    console_print("\b \b");
+                    break;
+                default:
+                    console_print("%c", r1);
+            }
+        } break;
         case 1:
             return hal_io_serial_getc();
         default:
