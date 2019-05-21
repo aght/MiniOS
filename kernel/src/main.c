a35bca4f074b909039b98b76da3b4cff892245f1
Andy Tang <andytang43@gmail.com>
2019-05-21 11:42:51 -0700

    changes to rectangle drawing

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -7,6 +7,36 @@
 #include "console/console.h"
 #include "rpi/video/rpi_video.h"
 
+typedef struct {
+    int x;
+    int y;
+    int w;
+    int h;
+    int xdir;
+    int ydir;
+} rect;
+
+void update_rect(rect* rect) {
+    rect->x += rect->xdir;
+    rect->y += rect->ydir;
+
+    if (rect->x <= 0) {
+        rect->xdir *= -1;
+    }
+
+    if (rect->x + rect->w >= 640) {
+        rect->xdir *= -1;
+    }
+
+    if (rect->y <= 0) {
+        rect->ydir *= -1;
+    }
+
+    if (rect->y + rect->h >= 480) {
+        rect->ydir *= -1;
+    }
+}
+
 void main(void) {
     rpi_video_init(640, 480, 24);
     hal_io_serial_init();
@@ -17,38 +47,19 @@ void main(void) {
 
     int xdir = 1;
     int ydir = 1;
-    int x = 0;
-    int y = 0;
-    const int w = 230;
-    const int h = 100;
+
+    rect rect1 = {0, 0, 230, 100, 1, 1};
+    rect rect2 = {50, 100, 50, 60, -1, 1};
 
     rgb_t red = {255, 0, 0};
     rgb_t green = {0, 255, 0};
 
-    bool flip = true;
-
     while (1) {
-        
-        rpi_video_rect(x, y, w, h, green);
+        rpi_video_rect(rect1.x, rect1.y, rect1.w, rect1.h, green);
+        rpi_video_rect(rect2.x, rect2.y, rect2.w, rect2.h, red);
 
-        x += xdir;
-        y += ydir;
-
-        if (x <= 0) {
-            xdir *= -1;
-        }
-
-        if (x + w >= 640) {
-            xdir *= -1;
-        }
-
-        if (y <= 0) {
-            ydir *= -1;
-        }
-
-        if (y + h >= 480) {
-            ydir *= -1;
-        }
+        update_rect(&rect1);
+        update_rect(&rect2);
 
         rpi_video_swap_buffer();
         rpi_video_clear();
